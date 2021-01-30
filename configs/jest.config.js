370dc9792791f4f8aae1d0fa5bb72bb3eb945d10
lancetipton <lancetipton04@gmail.com>
2021-01-30 02:56:03 -0700

    feat(cli-utils): Added formattng and test configs

---
+++ configs/jest.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  testEnvironment: "node",
+  verbose: true,
+  globals: {
+    __DEV__: true
+  },
+  moduleNameMapper: {},
+  testMatch: [
+    "<rootDir>/src/**/__tests__/**/*.js?(x)",
+    "<rootDir>/scripts/**/__tests__/**/*.js?(x)"
+  ],
+  transformIgnorePatterns: [
+    ".*"
+  ],
+  collectCoverageFrom: [
+    "src/**/*.{js}",
+    "!**/__mocks__/**/*.{js}"
+  ],
+  coverageDirectory: "reports/coverage",
+  setupFilesAfterEnv: []
+}
